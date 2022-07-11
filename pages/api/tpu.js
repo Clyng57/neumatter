878c624df8fc0ff05e890f79b21c923d95a930b4
Clyng57 <clyng57@gmail.com>
2022-07-11 12:51:09 -0500

    Update tpu.js

--- pages/api/tpu.js
+++ pages/api/tpu.js
@@ -1,27 +1,20 @@
 import path from 'path'
-import f from 'fs'
+import f from 'fs/promises'
 import { convert } from 'convert-svg-to-png'
 
-export default function (req, res) {
-  return new Promise((resolve, reject) => {
+export default async function sendFile (req, res) {
     console.log(req.url)
     const filePath = path.join(process.cwd(), 'public/tpu-logo.svg');     
     const fileName = filePath.substring(filePath.lastIndexOf("/") + 1).replace('.svg', '.png');
-    f.readFile(filePath, (err, fileBuffer) => {
-      const jsonBody = JSON.parse(JSON.stringify(req.body))
-      const color = `fill="${jsonBody.color}"`
-      console.log(color)
-      const fileString = fileBuffer.toString().replace(/fill="[^"]*"/g, color)
-      const data = fileString
-      convert(data, { width: 1273, height: 640 })
-        .then(async fileData => {
-          res.status(200)
-          const stats = f.statSync(filePath)
-          res.setHeader('Content-Length', stats.size)
-          res.setHeader('Content-Disposition',`attachment; filename=${fileName}`);
-          await res.send(fileData)
-          resolve(true)
-        })
+    const fileBuffer = await f.readFile(filePath)
+    const jsonBody = JSON.parse(JSON.stringify(req.body))
+    const color = `fill="${jsonBody.color}"`
+    const fileString = fileBuffer.toString().replace(/fill="[^"]*"/g, color)
+    const data = fileString
+    const fileData = await convert(data, { width: 1273, height: 640 })
+    res.status(200)
+    res.setHeader('Content-Disposition',`attachment; filename=${fileName}`);
+    await res.send(fileData)
         /*
       f.writeFile(filePath, data, (err) => {
         if (err) throw err
@@ -32,6 +25,4 @@ export default function (req, res) {
           })
       })
       */
-    })
-  })
 }
