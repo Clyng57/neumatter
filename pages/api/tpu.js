283d2c1bea5c469c35bfe7c116799756b8e5e938
Clyng57 <clyng57@gmail.com>
2022-07-11 11:55:15 -0500

    tpu-2

--- pages/api/tpu.js
+++ pages/api/tpu.js
@@ -13,6 +13,7 @@ export default (req, res)=> {
     const data = fileString
     convert(data, { width: 1273, height: 640 })
       .then(fileData => {
+        res.status(200)
         res.setHeader('Content-Disposition',`attachment; filename=${fileName}`);
         res.end(fileData)
       })
