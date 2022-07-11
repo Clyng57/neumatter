f827313a85a3daa3797771591670fb6ec99f013e
Clyng57 <clyng57@gmail.com>
2022-07-11 13:42:43 -0500

    Update tpu.js

--- pages/api/tpu.js
+++ pages/api/tpu.js
@@ -6,12 +6,12 @@ export default async function sendFile (req, res) {
     console.log(req.url)
     const filePath = path.join(process.cwd(), '_packages/tpu-logo.svg');     
     const fileName = filePath.substring(filePath.lastIndexOf("/") + 1).replace('.svg', '.png');
+    res.setHeader('Content-Disposition',`attachment; filename=${fileName}`)
     const fileBuffer = await f.readFile(filePath)
     const jsonBody = JSON.parse(JSON.stringify(req.body))
     const color = `fill="${jsonBody.color}"`
     const fileString = fileBuffer.toString().replace(/fill="[^"]*"/g, color)
     const data = fileString
     const fileData = await convert(data, { width: 1273, height: 640 })
-    // res.setHeader('Content-Disposition',`attachment; filename=${fileName}`)
-    res.status(200).json(fileData)
+    await res.status(200).send(fileData)
 }
